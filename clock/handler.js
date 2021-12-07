594bdbff83b56f278b4a733dcb89eeab3eb4590a
Tekalegn Fisseha <tekalegn@minwo.co>
2021-12-07 09:04:50 -0500

    clock function modified

--- clock/handler.js
+++ clock/handler.js
@@ -3,10 +3,10 @@ const moment = require("moment");
 exports.clock = async (event) => {
   console.log("Clock function run");
 
-  const message = moment().format();
+  const message = moment().format() + "Teka";
   const response = {
     statusCode: 200,
     body: JSON.stringify(message),
   };
-  return response + "Teka";
+  return response;
 };
